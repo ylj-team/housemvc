9bc6a4e101e5a9cecd66adb204c81e5447d72a91
Administrator <Administrator@USER-20141215CG>
2015-12-20 22:49:29 +0800

    some modfiy

--- src/main/java/ylj/house/mvc/controllers/UserIndxController.java
+++ src/main/java/ylj/house/mvc/controllers/UserIndxController.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ylj.house.tmsf.data.salestate.PropertyDailySigned;
 import ylj.house.tmsf.data.salestate.PropertyDailySignedAffairs;
@@ -27,16 +28,15 @@ import ylj.house.tmsf.data2.DaySaledHouseAffairs;
 import ylj.house.user.subscription.UserPropertySubscription;
 import ylj.house.user.subscription.UserPropertySubscriptionAffairs;
 
-
 @Controller("UserIndxController")
 public class UserIndxController {
 
 	static Logger logger = LoggerFactory.getLogger(UserIndxController.class);
 
-	private void setRedirectToLogin(HttpServletResponse httpResponse, String nextUrl,String message) throws IOException {
+	private void setRedirectToLogin(HttpServletResponse httpResponse, String nextUrl, String message) throws IOException {
 		//
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		
+
 		// errorMessage=errorMessage.replaceAll(" ", "%20");
 		params.add(new BasicNameValuePair("message", message));
 		params.add(new BasicNameValuePair("nextUrl", nextUrl));
@@ -60,11 +60,11 @@ public class UserIndxController {
 		httpResponse.sendRedirect(reloginUrl);
 	}
 
-	public static class SubscribedProperty extends UserPropertySubscription{
-		
-		 String propertyId;
-		 
-		 public String getPropertyId() {
+	public static class SubscribedProperty extends UserPropertySubscription {
+
+		String propertyId;
+
+		public String getPropertyId() {
 			return propertyId;
 		}
 
@@ -81,7 +81,7 @@ public class UserIndxController {
 		}
 
 		String propertyName;
-		 int signNumber;
+		int signNumber;
 
 		public int getSignNumber() {
 			return signNumber;
@@ -90,66 +90,97 @@ public class UserIndxController {
 		public void setSignNumber(int signNumber) {
 			this.signNumber = signNumber;
 		}
-		
-		
+
 	}
-	
-	private int maxSignedNumber(List<PropertyDailySigned> signeds){
-		int maxSignedNumber=0;		
-		for(PropertyDailySigned signed:signeds){
-			int signedNumber=Integer.parseInt(signed.getSignedNumber()) ;
-			if(signedNumber>maxSignedNumber)
-				maxSignedNumber=signedNumber;
+
+	private int maxSignedNumber(List<PropertyDailySigned> signeds) {
+		int maxSignedNumber = 0;
+		for (PropertyDailySigned signed : signeds) {
+			int signedNumber = Integer.parseInt(signed.getSignedNumber());
+			if (signedNumber > maxSignedNumber)
+				maxSignedNumber = signedNumber;
 		}
-		
+
 		return maxSignedNumber;
 	}
-	
-	@RequestMapping(value = "/userIdx", method = { RequestMethod.POST, RequestMethod.GET })
-	public String handleUser(HttpSession session, Model model,HttpServletResponse httpResponse) throws Exception {
-		
-			
-			Boolean login = (Boolean) session.getAttribute("login");
-			String loginedAccount = (String) session.getAttribute("account");
 
-			// already logined
-			if (login != null && login == true) {
-				
-				model.addAttribute("account", loginedAccount);		
-				
-				List<SubscribedProperty> subscriptions=new LinkedList<SubscribedProperty>();
-				List<UserPropertySubscription> subs=UserPropertySubscriptionAffairs.getUserSubscriptions(loginedAccount);
-			
-				
-				if(subs.size()>0){
-					SimpleDateFormat ISO_time_format= new SimpleDateFormat("yyyy-MM-dd");;
-					String dateStr=ISO_time_format.format(new Date(System.currentTimeMillis()));
+	@RequestMapping(value = "/userIdxFastPath", method = { RequestMethod.POST, RequestMethod.GET })
+	public String handleUserFastPath(@RequestParam(value = "account", required = true) String account, HttpSession session, Model model,
+			HttpServletResponse httpResponse) throws Exception {
 
-					for(UserPropertySubscription sub:subs){
-						
-						SubscribedProperty subscription=new SubscribedProperty();
-						
-						int signNumber=maxSignedNumber(PropertyDailySignedAffairs.querySaledHouseBetween(sub.getPropertyId(), dateStr, dateStr));
-						
-						subscription.setPropertyId(sub.getPropertyId());
-						subscription.setPropertyName(sub.getPropertyName());
-						subscription.setSignNumber(signNumber);
-						
-						subscriptions.add(subscription);
-					}
-					
-					
-				}
-				
-				
-				model.addAttribute("subscriptions", subscriptions);		
-				
-				
-				return "user_index_jstl";
-			}else{
-				setRedirectToLogin( httpResponse,"/userIdx","未登陆");
-				return null;
+		session.setAttribute("login", true);
+		session.setAttribute("account", account);
+
+		// already logined
+		List<SubscribedProperty> subscriptions = new LinkedList<SubscribedProperty>();
+		List<UserPropertySubscription> subs = UserPropertySubscriptionAffairs.getUserSubscriptions(account);
+
+		if (subs.size() > 0) {
+			SimpleDateFormat ISO_time_format = new SimpleDateFormat("yyyy-MM-dd");
+			String dateStr = ISO_time_format.format(new Date(System.currentTimeMillis()));
+
+			for (UserPropertySubscription sub : subs) {
+
+				SubscribedProperty subscription = new SubscribedProperty();
+
+				int signNumber = maxSignedNumber(PropertyDailySignedAffairs.querySaledHouseBetween(sub.getPropertyId(), dateStr, dateStr));
+
+				subscription.setPropertyId(sub.getPropertyId());
+				subscription.setPropertyName(sub.getPropertyName());
+				subscription.setSignNumber(signNumber);
+
+				subscriptions.add(subscription);
 			}
-					
+
+		}
+		
+		model.addAttribute("account", account);
+		model.addAttribute("subscriptions", subscriptions);
+
+		return "user_index_jstl";
+	}
+
+	@RequestMapping(value = "/userIdx", method = { RequestMethod.POST, RequestMethod.GET })
+	public String handleUser(HttpSession session, Model model, HttpServletResponse httpResponse) throws Exception {
+
+		Boolean login = (Boolean) session.getAttribute("login");
+		String loginedAccount = (String) session.getAttribute("account");
+
+		// already logined
+		if (login != null && login == true) {
+
+			model.addAttribute("account", loginedAccount);
+
+			List<SubscribedProperty> subscriptions = new LinkedList<SubscribedProperty>();
+			List<UserPropertySubscription> subs = UserPropertySubscriptionAffairs.getUserSubscriptions(loginedAccount);
+
+			if (subs.size() > 0) {
+				SimpleDateFormat ISO_time_format = new SimpleDateFormat("yyyy-MM-dd");
+				;
+				String dateStr = ISO_time_format.format(new Date(System.currentTimeMillis()));
+
+				for (UserPropertySubscription sub : subs) {
+
+					SubscribedProperty subscription = new SubscribedProperty();
+
+					int signNumber = maxSignedNumber(PropertyDailySignedAffairs.querySaledHouseBetween(sub.getPropertyId(), dateStr, dateStr));
+
+					subscription.setPropertyId(sub.getPropertyId());
+					subscription.setPropertyName(sub.getPropertyName());
+					subscription.setSignNumber(signNumber);
+
+					subscriptions.add(subscription);
+				}
+
+			}
+
+			model.addAttribute("subscriptions", subscriptions);
+
+			return "user_index_jstl";
+		} else {
+			setRedirectToLogin(httpResponse, "/userIdx", "未登陆");
+			return null;
+		}
+
 	}
 }
